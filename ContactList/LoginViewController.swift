1afe7db95fb056ee69f126171cef18ff65608137
Andrea Prearo <andrea.prearo@gmail.com>
2016-03-05 10:47:45 -0800

    Initial Commit

---
+++ ContactList/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.swift
+//  ContactList
+//
+//  Created by Prearo, Andrea on 3/5/16.
+//  Copyright © 2016 Prearo, Andrea. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '8.0'
+ 
+# inhibit_all_warnings!
+use_frameworks!
+ 
+target 'ContactList' do
+  pod 'Alamofire', '~> 3.2.1'
+end
