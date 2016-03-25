959d84a5b234fb4aa2beda08fb0d1ce4fd748ae5
Andrea Prearo <andrea.prearo@gmail.com>
2016-03-25 16:35:03 -0700

    Update default avatar image

--- ContactList/UIImageView+Util.swift
+++ ContactList/UIImageView+Util.swift
@@ -10,33 +10,48 @@ import UIKit
 
 extension UIImageView {
 
-    func downloadImageFromUrl(url: String) {
+    class func defaultAvatarImage() -> UIImage? {
+        return UIImage(named: "Avatar")
+    }
+
+    func downloadImageFromUrl(url: String, defaultImage: UIImage? = UIImageView.defaultAvatarImage()) {
         guard let url = NSURL(string: url)
         else {
+            setRoundedImage(defaultImage)
             return
         }
-        NSURLSession.sharedSession().dataTaskWithURL(url, completionHandler: { (data, response, error) -> Void in
+        NSURLSession.sharedSession().dataTaskWithURL(url, completionHandler: { [weak self] (data, response, error) -> Void in
             guard let httpURLResponse = response as? NSHTTPURLResponse where httpURLResponse.statusCode == 200,
                 let mimeType = response?.MIMEType where mimeType.hasPrefix("image"),
                 let data = data where error == nil,
                 let image = UIImage(data: data)
             else {
+                self?.setRoundedImage(defaultImage)
                 return
             }
-            dispatch_async(dispatch_get_main_queue()) { [weak self] () -> Void in
-                self?.image = image
-                if let width = self?.frame.size.width {
-                    self?.roundedImage(width * CGFloat(0.5))
-                }
-            }
+            self?.setRoundedImage(image)
         }).resume()
     }
 
-    private func roundedImage(cornerRadius: CGFloat) {
+    private func setRoundedImage(image: UIImage?) {
+        guard let image = image else {
+            return
+        }
+        dispatch_async(dispatch_get_main_queue()) { [weak self] () -> Void in
+            self?.image = image
+            if let width = self?.frame.size.width {
+                self?.roundedImage(width * CGFloat(0.5))
+            }
+        }
+    }
+
+    private func roundedImage(cornerRadius: CGFloat, withBorder: Bool = true) {
         layer.borderWidth = 1.0
         layer.masksToBounds = false
-        layer.borderColor = UIColor.whiteColor().CGColor
         layer.cornerRadius = cornerRadius
+        if withBorder {
+            layer.borderColor = UIColor.whiteColor().CGColor
+        }
         clipsToBounds = true
     }
 
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Sample app to interact with the [Node/Express/MongoDB web service example](https://github.com/andrea-prearo/node-contact-list-webservice)
 
 # Credits
-- Avatar icon made by [Freepik](http://www.freepik.com) from [Flaticon](http://www.flaticon.com) is licensed by [CC BY 3.0 BY](http://creativecommons.org/licenses/by/3.0/).
+- Default avatar based on avatar icon made by [Freepik](http://www.freepik.com) from [Flaticon](http://www.flaticon.com) is licensed by [CC BY 3.0 BY](http://creativecommons.org/licenses/by/3.0/).
